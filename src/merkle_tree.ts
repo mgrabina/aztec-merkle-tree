58b1dfbf55f76d4a4ce4b15f8c5b6997f1ea2375
Martin Grabina <mgrabina@itba.edu.ar>
2025-01-15 11:12:17 -0300

    solution in 1h live coding

--- src/merkle_tree.ts
+++ src/merkle_tree.ts
@@ -1,6 +1,6 @@
-import { LevelUp, LevelUpChain } from 'levelup';
-import { HashPath } from './hash_path';
-import { Sha256Hasher } from './sha256_hasher';
+import { LevelUp, LevelUpChain } from "levelup";
+import { HashPath } from "./hash_path";
+import { Sha256Hasher } from "./sha256_hasher";
 
 const MAX_DEPTH = 32;
 const LEAF_BYTES = 64; // All leaf values are 64 bytes.
@@ -23,12 +23,43 @@ export class MerkleTree {
    * @param depth The depth of the tree, to be no greater than MAX_DEPTH.
    * @param root When restoring, you need to provide the root.
    */
-  constructor(private db: LevelUp, private name: string, private depth: number, root?: Buffer) {
+  constructor(
+    private db: LevelUp,
+    private name: string,
+    private depth: number,
+    root?: Buffer
+  ) {
     if (!(depth >= 1 && depth <= MAX_DEPTH)) {
-      throw Error('Bad depth');
+      throw Error("Bad depth");
     }
 
-    // Implement.
+    // [hash] : [left+right]
+    // [hash] : [parent+left+right]
+
+    if (root) {
+      this.root = root;
+    } else {
+      const nodes: Buffer[] = [];
+      const isLeaf = (i: number) => i === depth;
+      for (let i = depth; i >= 0; i--) {
+        nodes[i] = isLeaf(i)
+          ? this.hasher.hash(Buffer.alloc(64))
+          : this.hasher.compress(nodes[i + 1], nodes[i + 1]);
+
+        if (!isLeaf(i)) {
+          const hashKey = nodes[i];
+          const hashValue = Buffer.concat([nodes[i + 1], nodes[i + 1]]);
+
+          db.put(hashKey, hashValue).catch(() => {
+            throw Error("Failed to write to db");
+          });
+        }
+      }
+      this.writeMetaData().catch(() => {
+        throw Error("Failed to write metadata");
+      });
+      this.root = nodes[0];
+    }
   }
 
   /**
@@ -39,6 +70,9 @@ export class MerkleTree {
     const meta: Buffer = await db.get(Buffer.from(name)).catch(() => {});
     if (meta) {
       const root = meta.slice(0, 32);
+      
+      
+      
       const depth = meta.readUInt32LE(32);
       return new MerkleTree(db, name, depth, root);
     } else {
@@ -72,15 +106,86 @@ export class MerkleTree {
    *     d3:   [ ]         [ ]          [*]         [*]           [ ]         [ ]          [ ]        [ ]
    */
   async getHashPath(index: number) {
-    // Implement.
-    return new HashPath();
+    const hashPath: Buffer[][] = [];
+    const getHashPathRecursive = async (current: Buffer, depth: number) => {
+      if (this.isLeaf(depth)) {
+        // If leaf, return;
+
+        return;
+      }
+
+      // 1 if right, 0 if left. This works because we are traversing the tree from the root to the leaf.
+      const parent = (await this.db.get(current)) as Buffer; // todo: use type verifications
+      const left = parent.slice(0, 32);
+      const right = parent.slice(32, 64);
+      const shouldGoRight = (index >> (this.depth - depth - 1)) & 1;
+
+      if (shouldGoRight) {
+        await getHashPathRecursive(right, depth + 1);
+      } else {
+        await getHashPathRecursive(left, depth + 1);
+      }
+
+      hashPath.push([left, right]);
+    };
+
+    hashPath.forEach((hash) => {
+      
+    });
+
+    await getHashPathRecursive(this.root, 0);
+
+    return new HashPath(hashPath);
+  }
+
+  isLeaf(index: number) {
+    return this.depth == index;
   }
 
   /**
    * Updates the tree with `value` at `index`. Returns the new tree root.
    */
   async updateElement(index: number, value: Buffer) {
-    // Implement.
+    // Index de la hoja
+    // Encuentr la hoja
+    // Modifico
+    // Subo a modificar Padres
+    // Actualizo Root
+
+    const updateRecursive = async (current: Buffer, depth: number) => {
+      if (this.isLeaf(depth)) {
+        // If leaf, update the value
+        const newHash = this.hasher.hash(value);
+        return newHash;
+      }
+
+      // 1 if right, 0 if left. This works because we are traversing the tree from the root to the leaf.
+      const parent = (await this.db.get(current)) as Buffer; // todo: use type verifications
+      const left = parent.slice(0, 32);
+      const right = parent.slice(32, 64);
+      const shouldGoRight = (index >> (this.depth - depth - 1)) & 1;
+
+      let newChildHash: Buffer,
+        compressedHash: Buffer,
+        newParentDbValue: Buffer;
+      if (shouldGoRight) {
+        newChildHash = await updateRecursive(right, depth + 1);
+        compressedHash = this.hasher.compress(left, newChildHash);
+        newParentDbValue = Buffer.concat([left, newChildHash]);
+      } else {
+        newChildHash = await updateRecursive(left, depth + 1);
+        compressedHash = this.hasher.compress(newChildHash, right);
+        newParentDbValue = Buffer.concat([newChildHash, right]);
+      }
+
+      await this.db.put(compressedHash, newParentDbValue);
+
+      return compressedHash;
+    };
+
+    this.root = await updateRecursive(this.root, 0);
+    await this.writeMetaData()
+
     return this.root;
   }
 }
